5c343cc0229fab6bceb0ca590e84f1566cd1133d
Özgün Murat Arslantaş <ozgun@resmo.com>
2022-11-29 18:58:14 +0300

    Do not run sql query with empty string

--- extensions/resmo/src/query.tsx
+++ extensions/resmo/src/query.tsx
@@ -61,6 +61,7 @@ export default function Command() {
       sql: searchText,
       resourceGroupId: "",
     }),
+    execute: Boolean(searchText),
   });
 
   const resultsCanBeListed = data?.resultType === "TABLE";
